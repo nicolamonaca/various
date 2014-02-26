0031467b37d10734e6dfca27163d5d2e967526af
Nicola Lamonaca <nicola.lamonaca@gmail.com>
2014-02-26 03:16:40 +0100

    Create FailCaffe.java
    'FailCaffè' is a simple game created by Francesco Granato (aka Ciccio Grana) in order to decide who will brew coffee.
    
    Im this game, each player has a set of 3 dices, made by a tetrahedron (4 faces), an octahedron (8 faces) and a dodecahedron (12 faces). Each player rolls each dice three times, each time summing together his partial results. The player with the lowest score will make coffee. As simple as that!
    
    Francesco's original infographics describing the game is at http://goo.gl/FE3yNb. (Description in Italian).
    
    Here you can download the JAR and play with your friends: https://db.tt/NQrNWliF. Download the file on your desktop, open your console, mode in the Desktop folder and type: 
    
    java -jar FailCaffe.jar
    
    First make sure you have Java installed on your system.
    
    For an example, you can look at this picture: http://goo.gl/4zgyT0.

---
+++ FailCaffe.java
@@ -0,0 +1,109 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class FailCaffe
+{	
+	public int randInt(Moka id)
+	{
+		Random rand = new Random();
+		int randomNumb = -1;
+			
+		switch(id)
+		{
+			case ID12: randomNumb = rand.nextInt(12) + 1;
+						 break;
+							 
+			case ID8: randomNumb = rand.nextInt(8) + 1;
+						break;
+							
+			case ID4: randomNumb = rand.nextInt(4) + 1;
+				
+			default: break;
+		}
+		
+		//System.out.println("randomNumb: " + randomNumb);
+		//System.out.println("id: " + id);
+		
+		return randomNumb;
+	}
+	
+	public static int findSmallest(int[] array)
+	{
+		int smallest = array[0];
+		
+		for(int i = 1; i < array.length; i++)
+		{
+			if(array[i] < smallest)
+			{
+				smallest = array[i];
+			}
+		}
+		
+		return smallest;
+	}
+
+	
+	public static void main(String[] args)
+	{
+		final int trials = 3;
+		
+		System.out.print("Please enter the number of players: ");
+		Scanner scanner = new Scanner(System.in);
+		int numPlayers = scanner.nextInt();
+		System.out.println("");
+		
+		Player players[] = new Player[numPlayers];
+		int[] points = new int[numPlayers];
+		
+		for(int i = 0; i < players.length; i++)
+		{
+			// Creates a new player
+			players[i] = new Player();
+			
+			// Asks for his name
+			System.out.print("Please enter player #" + (i+1) + "'s name: ");
+			players[i].name = scanner.next();
+		}
+		
+		System.out.println("");
+		
+		FailCaffe fc = new FailCaffe();
+		
+		for(int i = 0; i < players.length; i++)
+		{
+			for(int j = 0; j < trials; j++)
+			{
+				for(Moka dice : Moka.values())
+				{
+					players[i].points += fc.randInt(dice);
+				}
+			}
+			
+			points[i] = players[i].points;
+			
+			System.out.println(players[i].name + " totalized " + players[i].points + " points.");
+		}
+		
+		int largest = findSmallest(points);
+		
+		for(int i = 0; i < players.length; i++)
+		{
+			if(players[i].points == largest)
+			{
+				System.out.println("\n" + players[i].name + " lost. Coffee, pleeease!");
+				break;
+			}
+		}
+	}
+}
+
+enum Moka
+{
+	ID12, ID8, ID4
+}
+
+class Player
+{
+	int points;
+	String name;
+}
